f3ecc55b08601979dbc10030f874368af90bec64
jakubwilk <jw@jakubwilk.pl>
2022-08-23 22:32:00 +0200

    Add basic model for magic

---
+++ src/updates/magic/models/magic.model.ts
@@ -0,0 +1,8 @@
+export enum MagicBadgesEnum {
+  // eslint-disable-next-line no-unused-vars
+  NEW = 'NEW',
+  // eslint-disable-next-line no-unused-vars
+  UPDATE = 'UPDATE',
+  // eslint-disable-next-line no-unused-vars
+  DELETE = 'DELETE',
+}

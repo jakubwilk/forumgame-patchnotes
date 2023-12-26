e917a9c1c0ce45e819f5ee1f1a5d33786ffeefe7
jakubwilk <jw@jakubwilk.pl>
2023-12-26 23:05:38 +0100

    Update index.css, wrapper and layout components

--- src/app/AppWrapper.tsx
+++ src/app/AppWrapper.tsx
@@ -1,5 +1,20 @@
-import App from './App'
+import { RouterProvider } from 'react-router-dom'
+import { ROUTER } from 'src/config/router.config'
+import { createTheme, MantineProvider } from '@mantine/core'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ConfigProvier } from 'src/core/context/ConfigProvider'
+
+const theme = createTheme({})
+const queryClient = new QueryClient()
 
 export function AppWrapper() {
-  return <App />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <MantineProvider defaultColorScheme={'dark'} theme={theme}>
+        <ConfigProvier>
+          <RouterProvider router={ROUTER} />
+        </ConfigProvier>
+      </MantineProvider>
+    </QueryClientProvider>
+  )
 }
---
+++ src/core/layouts/Layout.tsx
@@ -0,0 +1,29 @@
+import { Outlet } from 'react-router-dom'
+import { Header } from '../header/Header'
+import { useGetConfig } from '../api/useGetConfig'
+import { Fragment, useCallback, useEffect, useState } from 'react'
+import { useConfigContext } from '../hooks/useConfigContext'
+import { CURRENT_PATCH_VERSION } from '../utils/api.utils'
+import { HeaderVersion } from '../header/HeaderVersion'
+
+export function Layout() {
+  const { data, isLoading } = useGetConfig()
+  const { setConfig } = useConfigContext()
+  const [selectedVersion, setSelectedVersion] = useState<string>(data?.update.current || CURRENT_PATCH_VERSION)
+
+  const handleSelectVersion = useCallback((value: string) => setSelectedVersion(value), [setSelectedVersion])
+
+  useEffect(() => {
+    if (!isLoading && data) {
+      setConfig(data)
+    }
+  }, [isLoading, data, setConfig])
+
+  return (
+    <Fragment>
+      <Header handleSelectVersion={handleSelectVersion} isConfigLoading={isLoading} />
+      <HeaderVersion currentVersion={selectedVersion} />
+      <Outlet />
+    </Fragment>
+  )
+}

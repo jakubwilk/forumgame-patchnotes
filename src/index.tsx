aae0bb6fc481744b0a1fb2f10daf838fd79d62fa
jakubwilk <jw@jakubwilk.pl>
2023-12-26 21:53:42 +0100

    Reinstall mantine

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
 import reportWebVitals from './reportWebVitals'
 import { AppWrapper } from './app/AppWrapper'
+
+import '@mantine/core/styles.css'
+import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(

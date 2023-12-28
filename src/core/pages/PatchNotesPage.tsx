6485ef4b6c668b18bbffb958c69572099ab49adc
jakubwilk <jw@jakubwilk.pl>
2023-12-28 16:13:22 +0100

    Pass data from query

--- src/core/pages/PatchNotesPage.tsx
+++ src/core/pages/PatchNotesPage.tsx
@@ -1,7 +1,21 @@
+import { useMemo } from 'react'
+import { useGetPatchNotes } from '../api/useGetPatchNotes'
+import { NodeCategory } from '../nodes/NodeCategory'
+import { IPatchNode } from '../models/api.model'
+
 interface IProps {
   version: string
 }
 
 export function PatchNotesPage({ version }: IProps) {
-  return <p>{`Aktualizacja do wersji ${version}`}</p>
+  const updateFileNameBasedOnVersion = useMemo(() => version.replaceAll('.', '-'), [version])
+  const { data, isLoading } = useGetPatchNotes(updateFileNameBasedOnVersion)
+
+  return (
+    <main>
+      <div className={'container max-w-[1000px] mx-auto'}>
+        <div className={'p-8'}>{isLoading ? <p>{'Ładowanie...'}</p> : <NodeCategory data={data?.patch as Array<IPatchNode>} />}</div>
+      </div>
+    </main>
+  )
 }

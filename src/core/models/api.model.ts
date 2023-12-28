22e5af432d0579972310d7adb3596001ad63e7cf
jakubwilk <jw@jakubwilk.pl>
2023-12-28 16:13:11 +0100

    Add patch JSON, add new models and update query keys

--- src/core/models/api.model.ts
+++ src/core/models/api.model.ts
@@ -12,3 +12,47 @@ export interface IConfig {
   update: IConfigUpdate
   router: Array<IConfigRouter>
 }
+
+export enum IPatchNodeCategoryKeyEnum {
+  // eslint-disable-next-line no-unused-vars
+  CATEGORY = 'CATEGORY',
+  // eslint-disable-next-line no-unused-vars
+  ENTITY = 'ENTITY',
+  // eslint-disable-next-line no-unused-vars
+  ITEM = 'ITEM',
+}
+
+export enum IPatchNodesLabelEnum {
+  // eslint-disable-next-line no-unused-vars
+  NEW = 'NEW',
+  // eslint-disable-next-line no-unused-vars
+  REMOVE = 'REMOVE',
+}
+
+export interface IPatchNodesItem {
+  categoryKey: IPatchNodeCategoryKeyEnum
+  slug: string
+  description: string
+  link?: string
+}
+
+export interface IPatchNodes {
+  categoryKey: IPatchNodeCategoryKeyEnum
+  name: string
+  description?: string
+  link?: string
+  label?: IPatchNodesLabelEnum
+  imageUri?: string
+  nodes?: Array<IPatchNodesItem>
+}
+
+export interface IPatchNode {
+  categoryKey: IPatchNodeCategoryKeyEnum
+  name: string
+  nodes: Array<IPatchNodes>
+  description?: string
+}
+
+export interface IPatch {
+  patch: Array<IPatchNode>
+}
--- src/core/utils/api.utils.ts
+++ src/core/utils/api.utils.ts
@@ -1,1 +1,2 @@
 export const GET_CONFIG_KEY = 'GET_CONFIG'
+export const GET_PATCH_NOTES_KEY = 'GET_PATCH_NOTES_'

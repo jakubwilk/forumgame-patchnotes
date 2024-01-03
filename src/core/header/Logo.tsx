689fa975d102f4f99ab44c770ff105571ffb6a46
jakubwilk <jw@jakubwilk.pl>
2024-01-03 16:39:39 +0100

    Fix header link styling

--- src/core/header/Logo.tsx
+++ src/core/header/Logo.tsx
@@ -1,3 +1,11 @@
+import { Anchor } from '@mantine/core'
+import styles from '../styles/header.module.css'
+import clsx from 'clsx'
+
 export function Logo() {
-  return <p>{'Mage Guild Wars'}</p>
+  return (
+    <Anchor href={'https://www.google.com'} className={clsx('duration-100', styles.logo)}>
+      {'Mage Guild Wars'}
+    </Anchor>
+  )
 }
